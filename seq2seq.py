5691cf4468d87bd4903ba2965e325e8c721ca217
‘michaelzhhan1 <hanz@cs.cmu.edu>
2017-03-29 00:33:50 -0700

    modify

--- seq2seq.py
+++ seq2seq.py
@@ -181,7 +181,7 @@ valid_batches = BatchGenerator(valid_text, 1, 1)
 
 
 with graph.as_default():
-
+    # encoder layer
     # Input gate: input, previous output, and bias.
     ix_encoder=tf.Variable(tf.truncated_normal([vocabulary_size,num_nodes],-0.1,0.1))
     im_encoder = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
@@ -202,6 +202,26 @@ with graph.as_default():
     saved_output_encoder = tf.Variable(tf.zeros([batch_size, num_nodes]), trainable=False)
     saved_state_encoder = tf.Variable(tf.zeros([batch_size, num_nodes]), trainable=False)
 
+    # decoder layer
+    # Input gate: input, previous output, and bias.
+    ix_decoder = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
+    im_decoder = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
+    ib_decoder = tf.Variable(tf.zeros([1, num_nodes]))
+    # Forget gate: input, previous output, and bias.
+    fx_decoder = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
+    fm_decoder = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
+    fb_decoder = tf.Variable(tf.zeros([1, num_nodes]))
+    # Memory cell: input, state and bias.
+    cx_decoder = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
+    cm_decoder = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
+    cb_decoder = tf.Variable(tf.zeros([1, num_nodes]))
+    # Output gate: input, previous output, and bias.
+    ox_decoder = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
+    om_decoder = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
+    ob_decoder = tf.Variable(tf.zeros([1, num_nodes]))
+    # Variables saving state across unrollings.
+    saved_output_decoder = tf.Variable(tf.zeros([batch_size, num_nodes]), trainable=False)
+    saved_state_decoder = tf.Variable(tf.zeros([batch_size, num_nodes]), trainable=False)
 
     # Classifier weights and biases.
     w = tf.Variable(tf.truncated_normal([num_nodes, vocabulary_size], -0.1, 0.1))
@@ -215,7 +235,12 @@ with graph.as_default():
     sm_encoder = tf.concat([im_encoder, fm_encoder, cm_encoder, om_encoder],1)
     sb_encoder = tf.concat([ib_encoder, fb_encoder, cb_encoder, ob_encoder],1)
 
-    def lstm_cell1(i,o,state):
+    sx_decoder = tf.concat([ix_decoder, fx_decoder, cx_decoder, ox_decoder], 1)
+    sm_decoder = tf.concat([im_decoder, fm_decoder, cm_decoder, om_decoder], 1)
+    sb_decoder = tf.concat([ib_decoder, fb_decoder, cb_decoder, ob_decoder], 1)
+
+
+    def lstm_cell_encoder(i,o,state):
         '''
         gatex=tf.matmul(i,gate_x)
         gatem=tf.matmul(o,gate_m)
@@ -226,7 +251,26 @@ with graph.as_default():
         output_gate=gatex[:,3*num_nodes:4*num_nodes]+gatem[:,3*num_nodes:4*num_nodes]+gate_b[0,3*num_nodes:4*num_nodes]
         return output_gate*tf.tanh(state),state
         '''
-        smatmul = tf.matmul(i, sx) + tf.matmul(o, sm) + sb
+        smatmul = tf.matmul(i, sx_encoder) + tf.matmul(o, sm_encoder) + sb_encoder
+        smatmul_input, smatmul_forget, update, smatmul_output = tf.split(smatmul,4,1)
+        input_gate = tf.sigmoid(smatmul_input)
+        forget_gate = tf.sigmoid(smatmul_forget)
+        output_gate = tf.sigmoid(smatmul_output)
+        state = forget_gate * state + input_gate * tf.tanh(update) # final memory cell
+        return output_gate * tf.tanh(state), state   # hidden state,  final memory cell
+
+    def lstm_cell_decoder(i,o,state):
+        '''
+        gatex=tf.matmul(i,gate_x)
+        gatem=tf.matmul(o,gate_m)
+        input_gate=tf.sigmoid(gatex[:,:num_nodes]+gatem[:,:num_nodes]+gate_b[0,:num_nodes])
+        forget_gate=tf.sigmoid(gatex[:,num_nodes:2*num_nodes]+gatem[:,num_nodes:2*num_nodes]+gate_b[0,num_nodes:2*num_nodes])
+        update=gatex[:,2*num_nodes:3*num_nodes]+gatem[:,2*num_nodes:3*num_nodes]+gate_b[0,2*num_nodes:3*num_nodes]
+        state=forget_gate*state+input_gate*tf.tanh(update)
+        output_gate=gatex[:,3*num_nodes:4*num_nodes]+gatem[:,3*num_nodes:4*num_nodes]+gate_b[0,3*num_nodes:4*num_nodes]
+        return output_gate*tf.tanh(state),state
+        '''
+        smatmul = tf.matmul(i, sx_decoder) + tf.matmul(o, sm_decoder) + sb_decoder
         smatmul_input, smatmul_forget, update, smatmul_output = tf.split(smatmul,4,1)
         input_gate = tf.sigmoid(smatmul_input)
         forget_gate = tf.sigmoid(smatmul_forget)
@@ -235,18 +279,8 @@ with graph.as_default():
         return output_gate * tf.tanh(state), state   # hidden state,  final memory cell
 
 
-    def lstm_cell(i, o, state):
-        """Create a LSTM cell. See e.g.: http://arxiv.org/pdf/1402.1128v1.pdf
-        Note that in this formulation, we omit the various connections between the
-        previous state and the gates."""
-        input_gate = tf.sigmoid(tf.matmul(i, ix) + tf.matmul(o, im) + ib)
-        forget_gate = tf.sigmoid(tf.matmul(i, fx) + tf.matmul(o, fm) + fb)
-        update = tf.matmul(i, cx) + tf.matmul(o, cm) + cb
-        state = forget_gate * state + input_gate * tf.tanh(update)
-        output_gate = tf.sigmoid(tf.matmul(i, ox) + tf.matmul(o, om) + ob)
-        return output_gate * tf.tanh(state), state
 
-        # Input data.
+    # Input data.
 
 
     train_data = list()
@@ -257,12 +291,12 @@ with graph.as_default():
     train_labels = train_data[1:]  # labels are inputs shifted by one time step.
 
     # Unrolled LSTM loop.
-    outputs = list()
-    output = saved_output
-    state = saved_state
+    outputs_encoder = list()
+    output_encoder = saved_output_encoder
+    state_encoder = saved_state_encoder
     for i in train_inputs:
-        output, state = lstm_cell1(i, output, state)
-        outputs.append(output)
+        output_encoder, state_encoder = lstm_cell_encoder(i, output_encoder, state_encoder)
+        outputs_encoder.append(output_encoder)
 
     # State saving across unrollings.
     with tf.control_dependencies([saved_output.assign(output),
